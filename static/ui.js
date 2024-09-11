fbb832d9f9e3760af6b987940dc9d833b88fe5cb
EurekaSigma <developers@eurekasigma.com>
2024-09-11 14:01:45 +0200

    Demo with functional widgets to change parameters of the ND2. Changed gui.py back to latest.

--- static/ui.js
+++ static/ui.js
@@ -2,31 +2,45 @@
 const positionSlider = document.getElementById("position_slider");
 const channelSlider = document.getElementById("channel_slider");
 const timeframeSlider = document.getElementById("timeframe_slider");
+const particleSlider = document.getElementById("particle_slider");
 
 document.addEventListener("DOMContentLoaded", (event) => {
   let channel = document.getElementById("channel_slider");
   let channelValue = document.getElementById("channel_value");
+  let n_channels = channel.max;
 
   channel.addEventListener("input", function () {
-    channelValue.innerHTML = channel.value;
+    channelValue.innerHTML = `${channel.value}/${n_channels}`;
   });
 });
 
 document.addEventListener("DOMContentLoaded", (event) => {
   let position = document.getElementById("position_slider");
   let positionValue = document.getElementById("position_value");
+  let n_positions = parseInt(position.max) + 1;
 
   position.addEventListener("input", function () {
-    positionValue.innerHTML = position.value;
+    positionValue.innerHTML = `${position.value}/${n_positions - 1}`;
   });
 });
 
 document.addEventListener("DOMContentLoaded", (event) => {
   let timeframe = document.getElementById("timeframe_slider");
   let timeframeValue = document.getElementById("timeframe_value");
+  let n_frames = timeframe.max;
 
   timeframe.addEventListener("input", function () {
-    timeframeValue.innerHTML = timeframe.value;
+    timeframeValue.innerHTML = `${timeframe.value}/${n_frames}`;
+  });
+});
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  let particle = document.getElementById("particle_slider");
+  let particleValue = document.getElementById("particle_value");
+  let all_particles_len = parseInt(particle.max) + 1;
+
+  particle.addEventListener("input", function () {
+    particleValue.innerHTML = `${particle.value}/${all_particles_len - 1}`;
   });
 });
 
